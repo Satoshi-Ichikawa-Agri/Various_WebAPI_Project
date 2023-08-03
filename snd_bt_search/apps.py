17978a4a8d93a234145638cc1fd108f7406e5a49
Satoshi-Ichikawa-Agri <stayhungry.stayfoolish.satoshi@gmail.com>
2023-08-04 06:40:05 +0900

    app名の変更

--- snd_bt_search/apps.py
+++ snd_bt_search/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SaunaSearchConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "sauna_search"
+    name = "snd_bt_search"
